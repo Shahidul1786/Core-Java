eacae1a9229cb69417e1ea4af0a96eb588a46a5a
Shahidul1786 <stshahidislam1786@gmail.com>
2019-08-28 23:35:05 +0600

    compele garbage collection

--- CoreJava(Basic)/src/GarbageCollection/Main.java
+++ CoreJava(Basic)/src/GarbageCollection/Main.java
@@ -11,4 +11,29 @@ package GarbageCollection;
  */
 public class Main {
     
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new  Student();
+        
+        //set data
+        s1.setData(5, 7);
+        s2.setData(10, 12);
+        //show data
+       // s1.showData();
+        s2.showData();
+        
+        Student s3;
+        
+        //s1 is eligible for Garbage collection
+        
+        s3 = s1;
+        
+       // s3.showData();
+        s1 = null; //    -----Garbage collection-----
+       // s1.showData();
+        
+        s3.showData();
+        
+    }
+    
 }
---
+++ CoreJava(Basic)/src/GarbageCollection/Student.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GarbageCollection;
+
+/**
+ *
+ * @author Shahidul
+ */
+public class Student {
+    
+    int a;
+    int b;
+    
+    
+    public void setData(int c,int d){
+        
+        a=c;
+        b = d;
+        
+    }
+    
+    public void showData(){
+        System.out.println(" value of a and b = "+a+","+b);
+     
+    }
+   
+    
+    
+}
