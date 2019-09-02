859a3a9e97ff603983207aa9ec7276a1bb97eae1
Shahidul1786 <stshahidislam1786@gmail.com>
2019-09-02 21:45:02 +0600

    complete switch case

---
+++ CoreJava(Basic)/src/SwitchDemo/DigitSpelling.java
@@ -0,0 +1,48 @@
+
+package SwitchDemo;
+
+import java.util.Scanner;
+
+
+public class DigitSpelling {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int digit;
+        System.out.println("Enter digit :");
+        
+        digit =sc.nextInt();
+        
+        switch(digit){
+            
+            case 0:
+                System.out.println("Zero");
+                break;
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            case 4:
+                System.out.println("four");
+                break;
+            case 5:
+                System.out.println("five");
+                break;
+            default:
+                System.out.println("Not a digit");
+                
+                
+            
+            
+        }
+    }
+    
+    
+    
+}
