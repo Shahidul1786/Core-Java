063036398deb594fed121f1bf4cc907f6a8e16a2
Shahidul1786 <stshahidislam1786@gmail.com>
2019-09-04 21:50:16 +0600

    complete upto prime number number

---
+++ CoreJava(Basic)/src/CheckingCalculations/PrimeNumber.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CheckingCalculations;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Shahidul
+ */
+public class PrimeNumber {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter a positive number : ");
+        
+        int a  = sc.nextInt();
+        int c = 0;
+        
+        for (int i = a; i >= 1; i--) {
+            
+            if (a%i ==0) {
+                c++;
+                
+            }
+             
+        }
+        
+        if (c==2) {
+            System.out.println("Number is prime");
+            
+        }
+        else{
+              System.out.println("Number is not prime");
+        }
+        
+    }
+    
+}
